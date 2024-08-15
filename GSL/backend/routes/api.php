c45f068bc9d615851b7dec5aacf6c0cfc1568c5b
Badrnej <nejaa.badr@gmail.com>
2024-08-15 15:44:04 +0100

    register/login

--- GSL/backend/routes/api.php
+++ GSL/backend/routes/api.php
@@ -4,15 +4,8 @@ use App\Http\Controllers\AuthController;
 
 use App\Http\Controllers\ProductController;
 
-
-
-Route::get('/v', function () {
-    return response()->json(['message' => 'hello velo']);
-});
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::resource('products', ProductController::class);
 Route::middleware(['auth.jwt'])->group(function () {
     Route::resource('products', ProductController::class);
 });
--- GSL/backend/routes/web.php
+++ GSL/backend/routes/web.php
@@ -2,28 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AuthController;
-
-
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::resource('products', ProductController::class);
-Route::middleware(['auth.jwt'])->group(function () {
-    Route::resource('products', ProductController::class);
-});
-
-
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::middleware(['auth.jwt'])->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('user', [AuthController::class, 'getUser']);
-});
-
 
 Route::resource('products', ProductController::class);
 Route::get('/', function () {
     return view('welcome');
 });
-
-
